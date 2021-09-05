972f389432839818f15d86dfeb108f0c324273ae
alex <aliexpresall@inbox.ru>
2021-09-05 23:13:11 +0100

    Am lucrat cu vector2. Am adaugat/modificat functii Math,Distance.

--- js/Vectors/Vector2.js
+++ js/Vectors/Vector2.js
@@ -15,11 +15,6 @@ class Vector2 extends Vector {
   constructor(x =0, y=0) {
     super(x,y)
   }
-  /**Face ca coordinatele sa nu iasa din marimile vec*/
-  NormalizeInRange(vec) {
-    this._x = this._x % vec.x
-    this._x = this._x % vec.y
-  }
   /**
    * Setam x si y 
    * @param {number} x
@@ -34,70 +29,32 @@ class Vector2 extends Vector {
     return this
   }
   /**
-   * Normalizam Vectorul de la -1 pina la 1
-   * @returns {this}
+   * Distanta pina la vector
+   * @param {Vector2|Vector} vec 
+   * @returns {number}
    */
-  Normalize(){
-      this._x = this._x<-1?-1:this._x>1?1:this._x
-      this._y = this._y<-1?0:this._y>1?1:this._y
-      return this
+  Distance(vec){
+    return Math.sqrt((this._x - vec._x) ** 2 + (this._y - vec._y) ** 2)
   }
   /**
-   * Adunam vectorii
-   * @param {Vector|Vector2} vec 
-   * @returns {Vector2}
+   * @param {'*'|'/'|'+'|'-'|'%'|'**'|"round"|"ceil"|"floor"} opperation 
+   * @param {Vector|Vector2} vector 
    */
-  Plus(vec) {
-    return new Vector2(
-      this.x + vec.x,
-      this.y + vec.y
-    )
-  }
-  /**
-   * Scadem vectorii
-   * @param {Vector|Vector2} vec 
-   * @returns {Vector2}
-   */
-  Minus(vec){
-    return new Vector2(
-      this.x-vec.x,
-      this.y-vec.y
-    )
-  }
-  /**
-   * Inmultim vectorii
-   * @param {Vector|Vector2} vec 
-   * @returns {Vector2}
-   */
-  Summ(vec) {
-    return new Vector2(
-      this.x * vec.x,
-      this.y * vec.y
-    )
-  }
-  /**
-   * Inpartim vectorii
-   * @param {Vector|Vector2} vec 
-   * @returns {Vector2}
-   */
-  Divide(vec) {
-    return new Vector2(
-      this.x / vec.x,
-      this.y / vec.y
-    )
-  }
-  /**
-   * Cream vector2 din Touch
-   * @param {Touch} touch
-   * @param {'client'|'page'} type ce tip de pozitie luam mai multe in documentatie js
-   * @returns {Vector2}*/
-  static fromTouch(touch, type = 'client') {
-    if (type == 'client')
-      return new Vector2(touch.clientX, touch.clientY)
-    if (type == 'page')
-      return new Vector2(touch.pageX, touch.pageY)
-    else
-      throw new Error('type is unknow')
+  Math(opperation,vector){
+    let opp = opperation.match(/(\*|\+|\-|\/|\*\*|%)/)?.[0]
+    let x,y;
+    if(opp){
+      let oper = new Function(`return {x:${this._x}${opp}${vector._x},y:${this._y}${opp}${vector._y}}`)()
+      x = oper.x
+      y = oper.y
+    }
+    let math = opperation.match(/(round|floor|ceil)/)?.[0]
+    if(math){
+      let oper = new Function(`return {x:Math.${math}(${this._x}),y:Math.${math}(${this._y})}`)()
+      x = oper.x
+      y = oper.y
+    }
+    return new Vector2(x,y)
   }
 }
 
