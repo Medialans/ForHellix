683bfe3ddcaef6eb0498b54868f5a5dca56391d4
1998 <aliexpresall@inbox.ru>
2021-09-05 21:40:41 +0300

    Init 1998

---
+++ js/app.js
@@ -0,0 +1,43 @@
+let cellCount = new Vector2(6,6)
+let cellSize  = new Vector2(60,60)
+let fieldSize = cellCount.math("*",cellSize)
+let rnd = new Random(1998)
+let startCellCount = 36
+//init map
+let map = {
+  style:{
+    width:fieldSize.x+"px"
+  }
+}
+//generate elements
+let elements = []
+for(var i=0;i<cellCount.x*cellCount.y;i++){
+  let el = {
+    num:0,
+    style:{
+      width:cellSize.x+"px",
+      height:cellSize.y+"px"
+    }
+  }
+  elements.push(el)
+}
+
+console.time()
+//generate 2 number
+var rndelnun = rnd.nextRangeCount(0,elements.length-1,startCellCount)
+
+console.log(rndelnun);
+console.timeEnd()
+var study = new Vue({
+  el: "#vue-app",
+  data: {
+    map,
+    elements,
+    cellSize,
+    cellCount,
+    fieldSize
+  }
+});
+
+
+var touch = new InputTouch()
